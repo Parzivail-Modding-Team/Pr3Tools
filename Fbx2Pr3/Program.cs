770f101dd2df5681abdbc99a1625671dacceaa59
parzivail <colby.newman2000@gmail.com>
2019-06-06 22:17:22 -0400

    selecting bones by name

--- Fbx2Pr3/Program.cs
+++ Fbx2Pr3/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Assimp;
 
 namespace Fbx2Pr3
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            var importer = new AssimpContext();
+            var scene = importer.ImportFile(args[0]);
+
+            if (scene == null || !scene.HasMeshes)
+                return -1;
+
+            var objects = scene.RootNode;
+
+            var armature = GetObjectByName(objects, "Armature");
+
+            var meshParts = objects.Children.Where(node => node.Name != "Armature");
+
+            foreach (var meshPart in meshParts)
+            {
+                var relatedBone = GetObjectByName(armature, meshPart.Name);
+                Console.WriteLine($"Mesh {meshPart.Name} <-> Bone {relatedBone?.Name}");
+            }
+
+            return 0;
+        }
+
+        private static Node GetObjectByName(Node objects, string name)
+        {
+            foreach (var o in objects.Children)
+            {
+                var childrenResult = GetObjectByName(o, name);
+                if (childrenResult != null)
+                    return childrenResult;
+
+                if (o.Name == name)
+                    return o;
+            }
+
+            return null;
         }
     }
 }
