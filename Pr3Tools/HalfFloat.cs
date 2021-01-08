71d87d48e5a00882ac16226b4c296ff509114372
parzivail <colby.newman2000@gmail.com>
2021-01-08 12:44:15 -0500

    convert SINGLE to HALF in vertex data

---
+++ Pr3Tools/HalfFloat.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace Pr3Tools
+{
+/**
+ * Accepts various forms of a floating point half-precision (2 byte) number
+ * and contains methods to convert to a
+ * full-precision floating point number Float and Double instance.
+ * <p>
+ * This implemention was inspired by x4u who is a user contributing
+ * to stackoverflow.com.
+ * (https://stackoverflow.com/users/237321/x4u).
+ *
+ * @author dougestep
+ */
+public class HalfFloat
+{
+	private readonly short _halfPrecision;
+	private float? _fullPrecision;
+
+	/**
+	 * Creates an instance of the class from the supplied the supplied
+	 * byte array.  The byte array must be exactly two bytes in length.
+	 *
+	 * @param bytes the two-byte byte array.
+	 */
+	public HalfFloat(byte[] bytes)
+	{
+		if (bytes.Length != 2)
+			throw new ArgumentException("The supplied byte array must be exactly two bytes in length");
+
+		_halfPrecision = BitConverter.ToInt16(bytes);
+	}
+
+	/**
+	 * Creates an instance of this class from the supplied short number.
+	 *
+	 * @param number the number defined as a short.
+	 */
+	public HalfFloat(short number)
+	{
+		_halfPrecision = number;
+		_fullPrecision = ToFullPrecision();
+	}
+
+	/**
+	 * Creates an instance of this class from the supplied
+	 * full-precision floating point number.
+	 *
+	 * @param number the float number.
+	 */
+	public HalfFloat(float number)
+	{
+		if (number > short.MaxValue)
+			throw new ArgumentException("The supplied float is too large for a two byte representation");
+		if (number < short.MinValue)
+			throw new ArgumentException("The supplied float is too small for a two byte representation");
+
+		var val = FromFullPrecision(number);
+		_halfPrecision = (short)val;
+		_fullPrecision = number;
+	}
+
+	/**
+	 * Returns the half-precision float as a number defined as a short.
+	 *
+	 * @return the short.
+	 */
+	public short GetHalfPrecisionAsShort()
+	{
+		return _halfPrecision;
+	}
+
+	/**
+	 * Returns a full-precision floating pointing number from the
+	 * half-precision value assigned on this instance.
+	 *
+	 * @return the full-precision floating pointing number.
+	 */
+	public float GetFullFloat()
+	{
+		_fullPrecision ??= ToFullPrecision();
+		return _fullPrecision.Value;
+	}
+
+	/**
+	 * Returns the full-precision float number from the half-precision
+	 * value assigned on this instance.
+	 *
+	 * @return the full-precision floating pointing number.
+	 */
+	private float ToFullPrecision()
+	{
+		var mantisa = _halfPrecision & 0x03ff;
+		var exponent = _halfPrecision & 0x7c00;
+
+		if (exponent == 0x7c00)
+			exponent = 0x3fc00;
+		else if (exponent != 0)
+		{
+			exponent += 0x1c000;
+			if (mantisa == 0 && exponent > 0x1c400)
+				return BitConverter.Int32BitsToSingle((_halfPrecision & 0x8000) << 16 | exponent << 13);
+		}
+		else if (mantisa != 0)
+		{
+			exponent = 0x1c400;
+			do
+			{
+				mantisa <<= 1;
+				exponent -= 0x400;
+			}
+			while ((mantisa & 0x400) == 0);
+			mantisa &= 0x3ff;
+		}
+
+		return BitConverter.Int32BitsToSingle((_halfPrecision & 0x8000) << 16 | (exponent | mantisa) << 13);
+	}
+
+	/**
+	 * Returns the integer representation of the supplied
+	 * full-precision floating pointing number.
+	 *
+	 * @param number the full-precision floating pointing number.
+	 *
+	 * @return the integer representation.
+	 */
+	private int FromFullPrecision(float number)
+	{
+		var fbits = BitConverter.SingleToInt32Bits(number);
+		var sign = fbits >> 16 & 0x8000;
+
+		var val = (fbits & 0x7fffffff) + 0x1000;
+
+		if (val >= 0x47800000)
+		{
+			if ((fbits & 0x7fffffff) >= 0x47800000)
+			{
+				if (val < 0x7f800000)
+					return sign | 0x7c00;
+				return sign | 0x7c00 | (fbits & 0x007fffff) >> 13;
+			}
+			return sign | 0x7bff;
+		}
+		if (val >= 0x38800000)
+			return sign | val - 0x38000000 >> 13;
+		if (val < 0x33000000)
+			return sign;
+		val = (fbits & 0x7fffffff) >> 23;
+		return sign | ((fbits & 0x7fffff | 0x800000) + (0x800000 >> val - 102) >> 126 - val);
+	}
+}
+}
--- Pr3Tools/Program.cs
+++ Pr3Tools/Program.cs
@@ -1,82 +1,110 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
 
 namespace Pr3Tools
 {
-    class Program
-    {
-        static int Main(string[] args)
-        {
-            if (args.Length < 2)
-                return -1;
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length < 3)
+				return -1;
 
-            var collada = XElement.Parse(File.ReadAllText(args[0]));
+			var collada = XElement.Parse(File.ReadAllText(args[0]));
 
-            var scene = new GeometryLoader(collada).Load();
-            var outputFile = args[1];
+			var scene = new GeometryLoader(collada).Load();
 
-            if (scene == null)
-                return -2;
+			if (scene == null)
+				return -2;
 
-            var model = Pr3Model.FromCollada(scene);
+			var model = Pr3Model.FromCollada(scene);
 
-            WriteOutputFile(outputFile, model);
+			WriteModelFile(args[1], model);
+			WriteRiggingFile(args[2], model);
 
-            return 0;
-        }
+			return 0;
+		}
 
-        private static void WriteOutputFile(string outputFile, Pr3Model model)
-        {
-	        using var f = new BinaryWriter(File.Open(outputFile, FileMode.Create));
+		private static void WriteModelFile(string outputFile, Pr3Model model)
+		{
+			using var f = new BinaryWriter(File.Open(outputFile, FileMode.Create));
 
-	        const string magic = "PR3";
-	        const int version = 1;
+			const string magic = "PR3";
+			const int version = 2;
 
-	        var ident = magic.ToCharArray();
+			var ident = magic.ToCharArray();
 
-	        f.Write(ident);
-	        f.Write(version);
+			f.Write(ident);
+			f.Write(version);
 
-	        f.Write(model.Objects.Count);
+			f.Write(model.Objects.Count);
 
-	        foreach (var pr3Object in model.Objects)
-	        {
-		        f.WriteNtString(pr3Object.Name);
-		        f.WriteNtString(pr3Object.MaterialName);
-		        WriteMatrix4(f, pr3Object.TransformationMatrix);
-		        WriteLengthCodedVectors(f, pr3Object.Vertices);
-		        WriteLengthCodedVectors(f, pr3Object.Normals);
-		        WriteLengthCodedVectors(f, pr3Object.Uvs);
-		        WriteLengthCodedFaces(f, pr3Object.Faces);
-	        }
-        }
+			foreach (var pr3Object in model.Objects)
+			{
+				f.WriteNtString(pr3Object.Name);
+				f.WriteNtString(pr3Object.MaterialName);
+				WriteMatrix4(f, pr3Object.TransformationMatrix);
+				WriteLengthCodedVectors(f, pr3Object.Vertices);
+				WriteLengthCodedVectors(f, pr3Object.Normals);
+				WriteLengthCodedVectors(f, pr3Object.Uvs);
+				WriteLengthCodedFaces(f, pr3Object.Faces);
+			}
+		}
 
-        private static void WriteLengthCodedFaces(BinaryWriter f, List<Pr3FacePointer> faces)
-        {
-            f.Write(faces.Count);
-            foreach (var face in faces)
-            {
-                f.Write(face.A);
-                f.Write(face.B);
-                f.Write(face.C);
-            }
-        }
+		private static void WriteRiggingFile(string outputFile, Pr3Model model)
+		{
+			using var f = new BinaryWriter(File.Open(outputFile, FileMode.Create));
 
-        private static void WriteMatrix4(BinaryWriter f, List<float> matrix)
-        {
-            foreach (var value in matrix) f.Write(value);
-        }
+			const string magic = "PR3R";
+			const int version = 1;
 
-        private static void WriteLengthCodedVectors(BinaryWriter f, List<Vector3> v)
-        {
-            f.Write(v.Count);
-            foreach (var vector3 in v)
-            {
-                f.Write(vector3.X);
-                f.Write(vector3.Y);
-                f.Write(vector3.Z);
-            }
-        }
-    }
+			var ident = magic.ToCharArray();
+
+			f.Write(ident);
+			f.Write(version);
+
+			f.Write(model.Objects.Count);
+
+			foreach (var pr3Object in model.Objects)
+			{
+				f.WriteNtString(pr3Object.Name);
+				WriteMatrix4(f, pr3Object.TransformationMatrix);
+			}
+		}
+
+		private static void WriteLengthCodedFaces(BinaryWriter f, List<Pr3FacePointer> faces)
+		{
+			f.Write7BitEncodedInt(faces.Count);
+			foreach (var face in faces)
+			{
+				f.Write7BitEncodedInt(face.A);
+				f.Write7BitEncodedInt(face.B);
+				f.Write7BitEncodedInt(face.C);
+			}
+		}
+
+		private static void WriteMatrix4(BinaryWriter f, List<float> matrix)
+		{
+			foreach (var value in matrix) f.Write(value);
+		}
+
+		private static void WriteLengthCodedVectors(BinaryWriter f, List<Vector3> v)
+		{
+			f.Write7BitEncodedInt(v.Count);
+			foreach (var vector3 in v)
+			{
+				WriteAsHalf(f, vector3.X);
+				WriteAsHalf(f, vector3.Y);
+				WriteAsHalf(f, vector3.Z);
+			}
+		}
+
+		private static void WriteAsHalf(BinaryWriter bw, float f)
+		{
+			var half = new HalfFloat(f);
+			bw.Write(half.GetHalfPrecisionAsShort());
+		}
+	}
 }
